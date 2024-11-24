92f37a0771a1c2f36ec993345a3a24ee5d07a9fc
vernonthedev <techjaja2@gmail.com>
2024-11-24 03:18:15 -0800

    Implemented loan plans listing.

--- wiro/app/Orchid/Screens/LoanPlanListScreen.php
+++ wiro/app/Orchid/Screens/LoanPlanListScreen.php
@@ -3,6 +3,17 @@
 namespace App\Orchid\Screens;
 
 use Orchid\Screen\Screen;
+use Orchid\Screen\Fields\Input;
+use Orchid\Support\Facades\Layout;
+use Orchid\Screen\Fields\DateTimer;
+use Orchid\Screen\Fields\Textarea;
+use Orchid\Screen\Fields\Select;
+use Orchid\Screen\Actions\Link;
+use Orchid\Screen\Actions\ModalToggle;
+use Illuminate\Http\Request;
+use App\Models\LoanPlan;
+use Orchid\Support\Facades\Toast;
+use Orchid\Screen\TD;
 
 class LoanPlanListScreen extends Screen
 {
@@ -13,7 +24,9 @@ class LoanPlanListScreen extends Screen
      */
     public function query(): iterable
     {
-        return [];
+        return [
+            'loan_plans' => LoanPlan::latest()->get(),
+        ];
     }
 
     /**
@@ -33,7 +46,12 @@ class LoanPlanListScreen extends Screen
      */
     public function commandBar(): iterable
     {
-        return [];
+        return [
+            ModalToggle::make('Add Loan Plan')
+            ->modal('loanPlanModal')
+            ->method('save')
+            ->icon('plus'),
+        ];
     }
 
     /**
@@ -43,6 +61,57 @@ class LoanPlanListScreen extends Screen
      */
     public function layout(): iterable
     {
-        return [];
+        return [
+            Layout::table('loan_plans', [
+                TD::make('name', 'Plan Name'),
+                TD::make('interest_rate', 'Interest Rate (%)'),
+                TD::make('duration', 'Duration (Months)'),
+                // TD::make('actions', 'Actions')
+                //     ->render(function (LoanPlan $loanPlan) {
+                //         return Link::make('Edit')
+                //             ->route('platform.loan-plans.edit', $loanPlan->id);
+                //     }),
+            ]),
+    
+            Layout::modal('loanPlanModal', Layout::rows([ // Wrap input fields in Layout::rows()
+                Input::make('loan_plan.name')
+                    ->title('Plan Name')
+                    ->placeholder('Enter Loan Plan Name'),
+    
+                Input::make('loan_plan.interest_rate')
+                    ->title('Interest Rate')
+                    ->placeholder('Enter Interest Rate'),
+    
+                Input::make('loan_plan.duration')
+                    ->title('Duration')
+                    ->placeholder('Enter Duration'),
+            ]))
+                ->title('Create New Loan Plan')
+                ->applyButton('Add Loan Plan'),
+        ];
     }
+
+    
+    //lets make the descriptions
+    public function description(): ?string
+    {
+        return 'All Plans for the different Available loans';
+    }
+
+    //let's save our data to the database
+    public function save(Request $request, LoanPlan $loan_plan)
+    {
+        $validated = $request->validate([
+            'loan_plan.name' => 'required|string|max:255',
+            'loan_plan.interest_rate' => 'required|integer|min:0',
+            'loan_plan.duration' => 'required|string|max:255',
+        ]);
+
+        LoanPlan::create($validated['loan_plan']); 
+
+        Toast::success('Loan was created successfully!');
+
+        return redirect()->route('platform.loan.plans');
+    }
+
 }
